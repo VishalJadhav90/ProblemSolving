507adbe0006e152a5f8d5bea9b8630b9ba1bc52a
VishalJadhav90 <jadhav.vsh@gmail.com>
2019-11-11 23:19:34 +0530

    Find non divisible subset

---
+++ NonDivisibleSubset.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'nonDivisibleSubset' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY s
+#
+
+def nonDivisibleSubset(k, s):
+    # Write your code here
+    print(k)
+    print(s)
+
+if __name__ == '__main__':
+    nonDivisibleSubset(3, [1, 7, 2, 4])
